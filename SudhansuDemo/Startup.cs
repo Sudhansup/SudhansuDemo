acad139ee04911ff171208dcb303d1dd1dd01ed5
sudhansup <sudhansu63@gmail.com>
2015-07-07 20:47:20 +0530

    First commit
    Adding a demo project

---
+++ SudhansuDemo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SudhansuDemo.Startup))]
+namespace SudhansuDemo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
